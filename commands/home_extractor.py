b4c8977682b9edc6f2fce17b2f87dfdf824614f9
Edwin Baeza <edwinbaeza05@gmail.com>
2021-05-24 04:42:40 -0500

    Add limit max_items to HomeExtractor task

--- commands/home_extractor.py
+++ commands/home_extractor.py
@@ -1,17 +1,17 @@
 import logging
 from datetime import datetime, timezone
 
-from sqlalchemy import select, insert
-
 from models.home import Home
 from commands.base import Base
 
 from libs.database_controller import get_default_controller
 
+from libs.config import get_config
 
 class HomeExtractor(Base):
 
     def __init__(self, spider, **kwargs):
+        self.__config = get_config()
         self.logger = logging.getLogger()
         self.controller = get_default_controller()
         self.session = self.controller.get_session()
@@ -32,4 +32,8 @@ class HomeExtractor(Base):
         return self.session\
                     .query(Home)\
                     .filter(Home.extracted_at.is_(None) & Home.url.isnot(None))\
+                    .limit(self.__max_items)\
                     .all()
+
+    def __max_items(self):
+        self.__config.tasks.get("home_extractor", {}).get("max_items", 200)
